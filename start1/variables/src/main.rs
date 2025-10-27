c120adcde834503f550a3a7ce7552385a6ab5680
kudo07 <vknwl077@gmail.com>
2025-10-27 23:47:32 +0530

    chore:changes

--- start1/variables/src/main.rs
+++ start1/variables/src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
-    let mut n:i32=5;
-    n+=1;
+    let n:i32=5;
 
     let n:i32=5;
     let n:i32=n+1;
     // now n is the most recent one
+    // n completely shaodwed the other n's
+    println!("{n}");
+    {
+        // different scope
+        let n:i32=10;
+        println!("{n}");
+        // everythin rust returns a tyep whether explicitly mentions or not
+
+    }
+    let n={
+        let n=6;
+        n
+        // this will return n as 6
+    };
+
+    println!("{n}");
 }
+ 
