ffd0209fb22b75987e1e041f2178cd8d4fe08f6e
kudo07 <vknwl077@gmail.com>
2025-11-04 00:25:54 +0530

    library

---
+++ start1/authentication/src/lib.rs
@@ -0,0 +1,30 @@
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+pub fn greet_user(name:&str)->String{
+    format!("Hello {name}")
+}
+#[cfg(test)]
+
+mod tests{
+    use super::*;
+
+    use crate::greet_user;
+    #[test]
+
+    fn test_greet_user(){
+        assert_eq!("Hello Mosely",greet_user("Mosely"))
+    }
+}
