7843630765374918e521775fc0eea60bf26a33b1
kudo07 <vknwl077@gmail.com>
2025-11-09 16:07:36 +0530

    enumeratins

--- start1/authentication/src/lib.rs
+++ start1/authentication/src/lib.rs
@@ -18,21 +18,46 @@ pub fn greet_user(name:&str)->String{
 }
 // fn for login
 
-pub fn login(username:&str,password:&str)->bool{
-    username.to_lowercase()=="admin" && password=="password"
+// pub fn login(username:&str,password:&str)->bool{
+//     username.to_lowercase()=="admin" && password=="password"
+// }
+// make another enumerations
+
+#[derive(PartialEq,Debug)]
+pub enum LoginAction{
+    Granted(LoginRole),
+    Denied
 }
 
+#[derive(PartialEq,Eq,Debug)]
+pub enum LoginRole{
+    Admin,
+    User,
+    // Denied
+}
+
+pub fn login(username:&str, password:&str)->Option<LoginAction>{
+    let username=username.to_lowercase();
+    if username != "admin" && username != "bob"{
+        return None;
+    }
+    if username=="admin" && password=="password"{
+        Some(LoginAction::Granted(LoginRole::Admin))
+    }
+    else if username == "bob" && password == "password"{
+        Some(LoginAction::Granted(LoginRole::User))
+    }
+    else {
+        Some(LoginAction ::Denied)
+    }
+}
 pub fn read_line()->String{
     let mut input:String=String::new();
     std::io::stdin().read_line(&mut input).expect("Std not working");
     input.trim().to_string()
 }
 
-pub enum LoginAction{
-    Admin,
-    User,
-    Denied
-}
+
 
 #[cfg(test)]
 
@@ -47,8 +72,12 @@ mod tests{
     }
     #[test]
     fn test_login(){
-        assert!(login("admin","password"));
-        assert!(!login("admin","Password"));
-        assert!(!login("Admin","Password"));
+        // assert!(login("admin","password"));
+        // assert!(!login("admin","Password"));
+        // assert!(!login("Admin","Password"));
+        // assert_eq!(login("admin","password"),LoginAction::Admin);
+        // assert_eq!(login("bob","password"),LoginAction::User);
+    //      assert_eq!(login("admin", "wrong"), LoginAction::Denied);
+    // assert_eq!(login("wrong", "password"), LoginAction::Denied); 
     }
 }
--- start1/login/src/main.rs
+++ start1/login/src/main.rs
@@ -1,4 +1,4 @@
-use authentication::{greet_user,login,read_line};
+use authentication::{greet_user,login,read_line, LoginAction};
 // authentication is the library work here
 fn main() {
     // println!("{}",greet_user("Mosely"));
@@ -9,11 +9,31 @@ fn main() {
         let username:String=read_line();
         println!("Enter your password");
         let password:String=read_line();
-        if login(&username, &password) {
-            println!("Welcome");
-            break
+        // LoginAction::Granted(authentication::LoginRole::Admin) =>  println!("Admin"),
+        match login(&username, &password){
+             Some(LoginAction::Granted(role))=>{
+                match role{
+                    authentication::LoginRole::Admin=>println!("Admin"),
+                    authentication::LoginRole::User=>println!("User")
+                     
+
+                }
+                break;
+             }
+             Some(LoginAction::Denied)=>{
+                // Do nothing
+             }
+             None=>{
+                println!("New user system")
+             }
         }
-        else{
+        // if login(&username, &password) {
+        //     println!("Welcome");
+        //     break
+        // }
+        // we are not now return true or false
+        // pattern matching heavily compile time
+        // else{
             println!("Incorrect username or password");
             tries+=1;
             if tries>=3{
@@ -23,4 +43,4 @@ fn main() {
         }
     }
 
-}
+
